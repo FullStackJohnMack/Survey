c6c7d19849ebab94304d93c99c0a71b93a5092c1
John Mack <johnmack@Johns-iMac-2.local>
2020-02-10 16:22:40 -0700

    added comments

--- app.py
+++ app.py
@@ -22,28 +22,32 @@ def start_survey():
 
 @app.route("/answer", methods=["POST"])
 def answer():
+    """Handles when a question is answered and adds to session."""
     answer = request.form["answer"]
     responses = session[RESPONSES_KEY]
     responses.append(answer)
     session[RESPONSES_KEY] = responses
+    # if routes user to next question
     if len(responses) < len(satisfaction_survey.questions):
         return redirect(f"questions/{len(responses)}")
+    # else redirects to "thank you" page upon completion of survey
     else:
         return redirect("/complete")
 
 
 @app.route("/questions/<num>")
 def question(num):
+    """Handles a what question comes next."""
     responses = session.get(RESPONSES_KEY)
-    # correct question number
+    # returns page for correct next question
     if int(num) == len(responses):
         question = satisfaction_survey.questions[int(num)].question
         choices = satisfaction_survey.questions[int(num)].choices
         return render_template("question.html", num=num, question=question, choices=choices)
-    # survey completed redirect
+    # returns survey complete page
     elif len(responses) == len(satisfaction_survey.questions):
         return redirect("/complete")
-    # redirects when the user trys to alter the URL
+    # returns redirects to correct page if the user trys to alter the URL
     else:
         flash(
             "You're trying to access an invalid URL; these questions must be done in order.")
@@ -52,5 +56,5 @@ def question(num):
 
 @app.route("/complete")
 def complete():
-    print(session[RESPONSES_KEY])
+    """Handles return of complete page."""
     return render_template("complete.html")
