9665d257d1568f1671f46748132c5ed5182bf1bd
John Mack <johnmack@Johns-iMac-2.local>
2020-02-10 15:03:24 -0700

    added flash message

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 # from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = "secret"
+app.config['SECRET_KEY'] = "secret"
 # debug = DebugToolbarExtension(app)
 
 responses = []
@@ -17,13 +17,18 @@ def main():
 # left off on flash messages
 @app.route("/questions/<num>")
 def question(num):
+    # correct question number
     if int(num) == len(responses):
         question = satisfaction_survey.questions[int(num)].question
         choices = satisfaction_survey.questions[int(num)].choices
         return render_template("question.html", num=num, question=question, choices=choices)
+    # survey completed redirect
     elif len(responses) == len(satisfaction_survey.questions):
         return redirect("/complete")
+    # redirects when the user trys to alter the URL
     else:
+        flash(
+            "You're trying to access an invalid URL; these questions must be done in order.")
         return redirect(f"{len(responses)}")
 
 
