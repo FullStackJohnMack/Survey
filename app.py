868c66066f6839418cae2550ad6391b1fd2fa1cf
John Mack <johnmack@Johns-iMac-2.local>
2020-02-10 16:08:36 -0700

    added sessions

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
-from flask import Flask, request, render_template, redirect, flash
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 
+
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "secret"
-# debug = DebugToolbarExtension(app)
 
-responses = []
+RESPONSES_KEY = "responses"
 
 
 @app.route("/")
 def main():
     return render_template("main.html", s=satisfaction_survey)
 
-# left off on flash messages
+
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    """Clear the session of responses."""
+    session[RESPONSES_KEY] = []
+    return redirect("/questions/0")
+
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    answer = request.form["answer"]
+    responses = session[RESPONSES_KEY]
+    responses.append(answer)
+    session[RESPONSES_KEY] = responses
+    if len(responses) < len(satisfaction_survey.questions):
+        return redirect(f"questions/{len(responses)}")
+    else:
+        return redirect("/complete")
+
+
 @app.route("/questions/<num>")
 def question(num):
+    responses = session.get(RESPONSES_KEY)
     # correct question number
     if int(num) == len(responses):
         question = satisfaction_survey.questions[int(num)].question
@@ -32,16 +50,7 @@ def question(num):
         return redirect(f"{len(responses)}")
 
 
-@app.route("/answer", methods=["POST"])
-def answer():
-    answer = request.form["answer"]
-    responses.append(answer)
-    if len(responses) < len(satisfaction_survey.questions):
-        return redirect(f"questions/{len(responses)}")
-    else:
-        return redirect("/complete")
-
-
 @app.route("/complete")
 def complete():
+    print(session[RESPONSES_KEY])
     return render_template("complete.html")
